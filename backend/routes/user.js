3fd62b5ba784932684ad3c49d2ccc64210b55515
bitsolidus <tommarlin587@gmail.com>
2026-03-15 23:13:14 +0100

    adminKYC.jsx

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -472,9 +472,11 @@ router.post('/kyc/submit',
 
       // Get file URLs if uploaded
       const files = req.files || {};
+      const backendUrl = process.env.BACKEND_URL || 'https://bitsolidus.tech';
       const getFileUrl = (fieldName) => {
         if (files[fieldName] && files[fieldName][0]) {
-          return `/uploads/kyc/${files[fieldName][0].filename}`;
+          // Return full URL instead of relative path
+          return `${backendUrl}/uploads/kyc/${files[fieldName][0].filename}`;
         }
         return null;
       };
